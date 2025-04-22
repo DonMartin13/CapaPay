0d9aa70540ebd8ca46df3e3ac26fc943845bfd4d
Yttsrei <delacruzkimberlyrei@gmail.com>
2025-04-22 16:01:00 +0800

    employee

--- src/app/Employee/employee-instant-send/employee-instant-send.page.ts
+++ src/app/Employee/employee-instant-send/employee-instant-send.page.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { IonApp, IonIcon, IonTabButton, IonLabel, IonFooter, IonContent, IonToolbar, IonTabs, IonTabBar, IonButton, IonList, IonListHeader, IonItem, IonFab, IonFabButton, IonHeader, IonTitle } from "@ionic/angular/standalone";
+import { Router } from '@angular/router';
+import { IonApp, IonIcon, IonTabButton, IonLabel, IonFooter, IonContent, IonToolbar, IonTabs, IonTabBar, IonButton, IonList, IonListHeader, IonItem, IonFab, IonFabButton, IonHeader, IonTitle, IonInput } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-employee-instant-send',
   templateUrl: './employee-instant-send.page.html',
   styleUrls: ['./employee-instant-send.page.scss'],
-  imports: [IonTitle, IonHeader, IonFabButton, IonFab, IonItem, IonListHeader, IonList, IonButton, IonApp, IonIcon, IonTabButton, IonLabel, IonFooter, IonContent, IonToolbar, IonTabs, IonTabBar] 
+  imports: [IonTitle, IonHeader, IonFabButton, IonFab, IonItem, IonListHeader, IonList, IonButton, IonApp, IonIcon, IonTabButton, IonLabel, IonFooter, IonContent, IonToolbar, IonTabs, IonTabBar, IonInput] 
 
 })
 export class EmployeeInstantSendPage implements OnInit {
 
-  constructor() { }
+    constructor(private router: Router ) { }
 
   ngOnInit() {
   }
-
+  goBackHome() {
+    this.router.navigate(['/employee-home']);
+  }
+  goToConfirmationPayment() {
+    this.router.navigate(['/employee-payment-confirmation']);
+  }
 }
--- src/app/Employee/employee-payment-confirmation/employee-payment-confirmation.page.ts
+++ src/app/Employee/employee-payment-confirmation/employee-payment-confirmation.page.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { IonApp, IonIcon, IonTabButton, IonLabel, IonFooter, IonContent, IonToolbar, IonTabs, IonTabBar, IonButton, IonList, IonListHeader, IonItem, IonFab, IonFabButton, IonHeader, IonTitle } from "@ionic/angular/standalone";
+import { Router } from '@angular/router';
+import { IonApp, IonIcon, IonTabButton, IonLabel, IonFooter, IonContent, IonToolbar, IonTabs, IonTabBar, IonButton, IonList, IonListHeader, IonItem, IonFab, IonFabButton, IonHeader, IonTitle, IonInput, IonCheckbox } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-employee-payment-confirmation',
   templateUrl: './employee-payment-confirmation.page.html',
   styleUrls: ['./employee-payment-confirmation.page.scss'],
-  imports: [IonTitle, IonHeader, IonFabButton, IonFab, IonItem, IonListHeader, IonList, IonButton, IonApp, IonIcon, IonTabButton, IonLabel, IonFooter, IonContent, IonToolbar, IonTabs, IonTabBar] 
+  imports: [IonTitle, IonHeader, IonFabButton, IonFab, IonItem, IonListHeader, IonList, IonButton, IonApp, IonIcon, IonTabButton, IonLabel, IonFooter, IonContent, IonToolbar, IonTabs, IonTabBar, IonCheckbox] 
 
 })
 export class EmployeePaymentConfirmationPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router ) { }
 
   ngOnInit() {
   }
-
+  goBackHome() {
+    this.router.navigate(['/employee-home']);
+  }
+  goToConfirmationPayment() {
+    this.router.navigate(['/employee-payment-confirmation']);
+  }
 }
+
+
